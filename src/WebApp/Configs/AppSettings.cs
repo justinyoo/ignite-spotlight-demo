66d49fd66a0be041b33d2267655bfb603309c97d
Justin Yoo <justin.yoojh@gmail.com>
2022-11-11 14:07:55 +0000

    Update Maps API

--- src/WebApp/Configs/AppSettings.cs
+++ src/WebApp/Configs/AppSettings.cs
@@ -3,20 +3,24 @@ namespace IgniteSpotlight.WebApp.Configs
     public class AppSettings
     {
         public virtual LinksSettings Links { get; set; }
-        public virtual EndpointSettings Endpoints { get; set; }
+        public virtual EndpointsSettings Endpoints { get; set; }
 
     }
 
     public class LinksSettings
     {
+        public const string Name = "Links";
+
         public virtual string Maps { get; set; }
         public virtual string Kakao { get; set; }
         public virtual string Sms { get; set; }
         public virtual string SmsVerify { get; set; }
     }
 
-    public class EndpointSettings
+    public class EndpointsSettings
     {
+        public const string Name = "Endpoints";
+
         public virtual string Maps { get; set; }
         public virtual string AccessToken { get; set; }
         public virtual string Profile { get; set; }
--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -7,8 +7,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => sp.GetService<IConfiguration>().Get<AppSettings>());
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped(sp => sp.GetService<IConfiguration>().GetSection(LinksSettings.Name).Get<LinksSettings>());
+builder.Services.AddScoped(sp => sp.GetService<IConfiguration>().GetSection(EndpointsSettings.Name).Get<EndpointsSettings>());
+
 builder.Services.AddGeolocationServices();
 
 await builder.Build().RunAsync();
