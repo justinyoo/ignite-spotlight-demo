7af69850208e475dcbf15b01482a1fece68c9daf
Justin Yoo <justin.yoojh@gmail.com>
2022-11-11 15:21:55 +0000

    Update Maps API

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -9,8 +9,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddScoped(sp => sp.GetService<IConfiguration>().GetSection(LinksSettings.Name).Get<LinksSettings>());
-builder.Services.AddScoped(sp => sp.GetService<IConfiguration>().GetSection(EndpointsSettings.Name).Get<EndpointsSettings>());
+// builder.Services.AddSingleton(sp => sp.GetService<IConfiguration>().GetSection(LinksSettings.Name).Get<LinksSettings>());
+// builder.Services.AddSingleton(sp => sp.GetService<IConfiguration>().GetSection(EndpointsSettings.Name).Get<EndpointsSettings>());
 
 builder.Services.AddGeolocationServices();
 
