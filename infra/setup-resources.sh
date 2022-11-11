215db0bba6a18868b723041089d311b60df3297a
Justin Yoo <justin.yoojh@gmail.com>
2022-11-11 06:06:40 +0000

    Fix typo on bicep

--- infra/setup-resources.sh
+++ infra/setup-resources.sh
@@ -6,5 +6,5 @@ set -e
 dispatched=$(curl -H "Accept: application/vnd.github+json" \
     -H "User-Agent: Autopilot" \
     -H "Authorization: token $GH_ACCESS_TOKEN" \
-    https://api.github.com/repos/justinyoo/ignite-spotlight-demo/actions/workflows/azure-dev.yaml/dispatches \
+    https://api.github.com/repos/justinyoo/ignite-spotlight-demo/actions/workflows/azure-dev.yml/dispatches \
     -d "{ \"ref\": \"main\", \"inputs\": { \"from_portal\": \"true\", \"azure_env_name\": \"$AZURE_ENV_NAME\", \"azure_location\": \"$AZURE_LOCATION\", \"azure_subscription_id\": \"$AZURE_SUBSCRIPTION_ID\" } }")
