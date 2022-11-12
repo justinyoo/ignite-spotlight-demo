8d80569043c8c428a15555c119359a8b38e0dabc
Justin Yoo <justin.yoojh@gmail.com>
2022-11-12 03:48:21 +0000

    Update WebApp

---
+++ src/WebApp/Models/KakaoProfileModel.cs
@@ -0,0 +1,9 @@
+namespace IgniteSpotlight.WebApp.Models
+{
+    public class KakaoProfileModel
+    {
+        public string NickName { get; set;}
+        public string ProfileImageURL { get; set; }
+        public string ThumbnailURL { get; set; }
+    }
+}
